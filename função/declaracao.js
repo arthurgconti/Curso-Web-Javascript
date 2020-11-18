53cce60f01b54bae6ea276fa0032e9053b6095d1
Arthur Conti <arthurgallotticonti@gmail.com>
2020-11-18 13:05:10 -0300

    função construtora e tipos de declaracoes

---
+++ função/declaracao.js
@@ -0,0 +1,19 @@
+console.log(soma(7,2))
+
+//function declaration
+//Pode se usar antes mesmo da declaração
+function soma(x, y) {
+    return x + y
+}
+
+//function expresison
+const sub = function (x, y) {
+    return x - y
+}
+console.log(sub(7,2))
+
+//named function expression
+const mult = function mult(x, y) {
+    return x * y
+}
+console.log(mult(7,2))
